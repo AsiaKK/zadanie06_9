4849e6a54d978120dff9a3ac188bec6b8b0ba28a
AsiaKK <joanna.kaminska83@gmail.com>
2019-01-27 19:13:42 +0100

    first commit

---
+++ src/CountsAndSum.java
@@ -0,0 +1,14 @@
+public class CountsAndSum {
+
+    public static void main(String[] args) {
+
+        int[] counts = new int[5];
+        counts[0] = 2;
+        counts[1] = 1;
+        counts[2] = 7;
+        counts[3] = 12;
+        counts[4] = 9;
+
+        System.out.println("SUMA liczby pierwszej, trzeciej i piatej WYNOSI =  " + (counts[0]+counts[2]+counts[4]));
+    }
+}
